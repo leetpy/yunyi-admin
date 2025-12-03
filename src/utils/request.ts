3729f57cb50be1c8bc4a8551c71ace2600db5122
yunyi <14208558+yunyi9023@user.noreply.gitee.com>
2025-12-03 17:09:35 +0800

    update requests

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,14 +1,22 @@
 import axios from 'axios'
+import type { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from "axios";
 
-const service = axios.create({
+
+const service: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_API,
   timeout: 99999
 })
 
 // http request 拦截器
-service.interceptors.request.use()
+service.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config
+  }
+)
 
 // http response 拦截器
-service.interceptors.response.use()
+service.interceptors.response.use(
+  async (response: AxiosResponse) => { }
+)
 
 export default service
